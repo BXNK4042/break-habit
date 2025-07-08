0156c88b597286f9a42987fda2dd0de0cc974642
BXNK4042 <parinthonkongthong@gmail.com>
2025-07-08 20:38:51 +0700

    remove unused feature

--- bot.py
+++ bot.py
@@ -21,26 +21,6 @@ async def on_message(message):
     now_time = datetime.datetime.now()
     now_time_formatted = now_time.strftime("%Y-%m-%d %H:%M")
 
-    log_entry = {
-        "author": message.author.name,
-        "content": message.content,
-        "timestamp": str(message.created_at)
-    }
-
-    def log_message():
-        if os.path.exists("log.json"):
-            with open("log.json", "r") as file:
-                logs = json.load(file)
-        else:
-            logs = []
-
-        logs.append(log_entry)
-        
-        with open("log.json", "w") as file:
-            json.dump(logs, file, indent=2)
-    
-    #log_message()
-
     def start_record():
         if os.path.exists("record.json"):
             with open("record.json", "r") as file:
@@ -62,9 +42,6 @@ async def on_message(message):
 
     if message.content.startswith('!hi'):
         await message.channel.send("Hello!")
-
-    if message.content.startswith('!time'):
-        await message.channel.send(str(now_time_formatted))
 
     if message.content.startswith('!gif'):
         giphy_key = os.getenv("GIPHY_KEY")
@@ -109,16 +86,5 @@ async def on_message(message):
                 data = json.load(file)
         else:
             await message.channel.send("You do a great job!")
-                
-# unused feature
-'''
-    if message.content.startswith("!quotes"):
-        with open("quotes.json", "r") as file:
-            data = json.load(file)
-            quote = random.choice(data["quotes"])
-            await message.channel.send(quote)
-'''
-
-
 
 client.run(os.getenv("DISCORD_TOKEN"))
